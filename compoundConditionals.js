9b9730d5ca10b1abc1d90a5b39ce9da80ae453f5
FarflungEyestrain <stedgegavin@gmail.com>
2024-06-17 15:56:14 -0400

    vjcdb ds

--- compoundConditionals.js
+++ compoundConditionals.js
@@ -2,8 +2,8 @@
 
 // 1. Create a secret username and password.
 //  - The variables are declared already. Update the values.
-let secretUsername = ("");
-let secretPassword = ("");
+let secretUsername = ("goooberstan_123");
+let secretPassword = ("hammerJamms");
 let button = document.querySelector("button");
 
 button.addEventListener("click", function() {
@@ -13,14 +13,21 @@ button.addEventListener("click", function() {
 
     // 2. Create a compound conditional statement to check for the following:
     //  - If the username AND password is correct, update the text of the message div to say "Success!"
+    if (username === secretUsername && password === secretPassword){
+        message.innerHTML = "Success!"
     
     // CODE SOLO
     // 3. Create additional compound conditionals that check for the following:
     //  - If the username is correct AND the password is incorrect, update the text of the message div to say "Wrong password - be careful, don't mess this up too many times."
+    }  else if (username === secretUsername && password !== secretPassword){
+    message.innerHTML = "Nuh uh - wrong password"
     //  - If the password is correct AND the username is incorrect, update the text of the message div to say "Wrong username - is there a typo?"
+    }  else if (username !== secretUsername && password === secretPassword){
+        message.innerHTML = "Wrong username - sus"
     //  - Finally, if the password is incorrect AND the username is incorrect, update the text of the message div to say "Incorrect username and password. Hmm. Suspicious."
-    
-    
+    } else if (username !== secretUsername && password !== secretPassword){
+        message.innerHTML = "Womp womp."
+    }
     // Bonus: If the username OR password fields are empty, update the text of the message div to say "You need to enter SOMETHING."
     //  - Hint: What string represents an "empty" message.
     
--- script.js
+++ script.js
@@ -5,11 +5,15 @@ button.addEventListener("click", function() {
     let waterTemp = document.querySelector("input").value;
     if (waterTemp >= 212) {
    document.querySelector("p").innerHTML="Boiling!";
-    }
+
     // 1. Add an else-if and else so that:
     //  - If the water temp is greater than 32°, the user knows it's ok to drink.
+     } else if (waterTemp > 32){
+        document.querySelector("p").innerHTML="It's ok to drink.";
     //  - If the water temp is 32° or less, the user knows the water is frozen.
-    
+     } else if (waterTemp < 32){
+        document.querySelector("p").innerHTML="FROZEN!";
+     }
     
     
 });
